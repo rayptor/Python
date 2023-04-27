383104f104098a22ce9ef6364d922f64762188e1
Peter <47330408+rayptor@users.noreply.github.com>
2023-04-27 18:07:15 +0200

    1st python

---
+++ solving nonlinear equation.py
@@ -0,0 +1,52 @@
+import numpy as np
+
+# A new class of optimal four-point methods with convergence order 16 for solving nonlinear equations
+def Solve(
+    f, df,
+    x0: np.longdouble,
+    k:int = 10,
+    tol: np.longdouble = 1e-8
+    ) -> np.longdouble:
+    dec = np.finfo(np.longdouble).precision
+    xo = x0
+    xn = xo
+    for _ in range(k):
+        fxn = f(xn)
+        if np.isclose(fxn, 0.0, atol=tol) == True:
+            break
+        dfxn = df(xn)
+        yn = np.longdouble(xo - fxn/dfxn)
+        fyn = f(yn)
+        tn = np.longdouble(fyn / fxn)
+        zn = np.longdouble(yn - ((1.0+tn**2) * (1.0+2.0*tn+2.0*tn**2) \
+            + tn**2*(2.0-8.0*tn-2.0*tn**2)) * fyn/dfxn)
+        fzn = f(zn)
+        un = np.longdouble(fzn / fxn)
+        sn = np.longdouble(fzn / fyn)
+        wn = np.longdouble(zn - (4.0*un-5.0*sn+(6.0+sn**3) \
+            * (tn**2+sn) + (1.0+un**3)*(1.0+2.0*tn)) * fzn/dfxn)
+        fwn = f(wn)
+        pn = np.longdouble(fwn / fxn)
+        qn = np.longdouble(fwn / fyn)
+        rn = np.longdouble(fwn / fzn)
+        xn1 = np.longdouble((1.0+tn) * (2.0*tn+tn**3) + 4.0*tn**2 \
+            - tn**3 - tn**4 - 2.0*sn**2)
+        xn2 = np.longdouble(2.0*tn*rn + 2.0*sn*un + 24.0*tn**4 + tn*un)
+        xn3 = np.longdouble((2.0*tn**3-10.0*tn*un**2+6.0*tn**2*un) \
+            / (1.0+2.0*tn*un) + ((1.0+2.0*pn+2.0*qn)/(1.0-rn)) \
+            + (6.0*pn)/(1.0+qn))
+        xn4 = np.longdouble((2.0*un+6*un**2)/(1.0+un) + (sn+2.0*sn**2) \
+            / (1.0+sn**2) + (6.0*tn**2*rn+6.0*tn**3*rn-4.0*sn**2*un) \
+            / (1.0+tn))
+        xn = np.longdouble(wn - (xn1-xn2+xn3-xn4) * fwn/dfxn)
+        if np.less(np.fabs(xn-xo), tol) == True:
+            break
+        xo = xn
+
+    return np.format_float_positional(np.longdouble(xn), \
+        unique=False, precision=dec)
+
+#if __name__ == "__name__":
+f = lambda x: x**7-np.sqrt(np.pi)*(2.0*x**2-4.0*x+2.0)+np.exp(x)-1.0
+df = lambda x: 7.0*x**6+ np.exp(x) - 4.0*np.sqrt(np.pi)*(x-1.0)
+print("x^7-np.sqrt(pi)*(2*x^2-4*x+2)+np.exp(x)-1, x =", Solve(f, df, 0.5))
